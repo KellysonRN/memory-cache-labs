5b98a38cd3b175d9d68c3dca03790ace47ea03e8
Kellyson Santos <kellyson.victor@gmail.com>
2023-10-22 12:03:18 -0300

    Set up ludopedia client

--- src/MemoryCache.Labs.API/Program.cs
+++ src/MemoryCache.Labs.API/Program.cs
@@ -1,4 +1,5 @@
 using MemoryCache.Labs.Infrastructure;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient(AppConstants.HTTP_CLIENT_NAME, httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://ludopedia.com.br/api/v1/");
+
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.Authorization, "Bearer " + builder.Configuration.GetValue<string>("LUDOPEDIA_SECRET"));
+    httpClient.DefaultRequestHeaders.Add(
+        HeaderNames.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0");
+});
 
 builder.Services.AddInfrastructureServices();
 
---
+++ src/MemoryCache.Labs.Infrastructure/AppConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace MemoryCache.Labs.Infrastructure;
+
+public class AppConstants
+{
+    public const string HTTP_CLIENT_NAME = "Ludopedia";
+}
