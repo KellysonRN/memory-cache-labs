3b60fcd3a1c0dc6841166efbe9a918d38f9518f0
Kellyson Santos <kellyson.victor@gmail.com>
2023-10-29 08:54:07 -0300

    Add new item inMemoryDatabase

---
+++ src/MemoryCache.Labs.InMemoryCopyDatabase/InMemoryDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using MemoryCache.Labs.Infrastructure.Exceptions;
+using MemoryCache.Labs.Infrastructure.Repositories;
+
+namespace MemoryCache.Labs.InMemoryCopyDatabase;
+
+public class InMemoryDatabase : IMemoryDatabase
+{
+    private readonly ConcurrentDictionary<object, BoardGameCacheRecord> _copyDatabase = new();
+    
+    public BoardGameCacheRecord AddData(object key, BoardGameCacheRecord data)
+    {
+        if (_copyDatabase.TryAdd(key, data))
+            return GetData(key);
+
+        throw new RecordAlreadyExistsException();
+    }
+
+    public bool ContainsData(object key)
+    {
+        return _copyDatabase.ContainsKey(key);
+    }
+
+    public BoardGameCacheRecord GetData(object key)
+    {
+        _copyDatabase.TryGetValue(key, out BoardGameCacheRecord? data);
+        
+        return data ?? throw new NotFoundRecordException();
+    }
+}
+
---
+++ tests/MemoryCache.Labs.Tests/DatabaseInMemoryTests.cs
@@ -0,0 +1,59 @@
+﻿using MemoryCache.Labs.Infrastructure.Exceptions;
+using MemoryCache.Labs.Infrastructure.Repositories;
+using MemoryCache.Labs.InMemoryCopyDatabase;
+
+namespace MemoryCache.Labs.Tests;
+
+public class DatabaseInMemoryTests
+{
+    [Fact]
+    public void AddData_ShouldAddCorrectly()
+    {
+        IMemoryDatabase inMemoryDatabase = new InMemoryDatabase();
+        
+        inMemoryDatabase.AddData(key: 1, data: new BoardGameCacheRecord(Id: 1, Name: "Board1", Link: "https://www.lipsum.com/"));
+
+        Assert.True(inMemoryDatabase.ContainsData(1));
+        Assert.Equal("Board1", inMemoryDatabase.GetData(1).Name);
+    }
+    
+    [Fact]
+    public void AddData_ShouldThrowException()
+    {
+        IMemoryDatabase inMemoryDatabase = new InMemoryDatabase();
+        
+        inMemoryDatabase.AddData(key: 1, data: new BoardGameCacheRecord(Id: 1, Name: "Board1", Link: "https://www.lipsum.com/"));
+
+        Assert.True(inMemoryDatabase.ContainsData(1));
+        
+        Assert.Throws<RecordAlreadyExistsException>(() =>
+        {
+            inMemoryDatabase.AddData(key: 1,
+                data: new BoardGameCacheRecord(Id: 1, Name: It.IsAny<string>(), Link: It.IsAny<string>()));
+        });
+    }
+    
+    [Fact]
+    public void DataRead_ShouldBeThreadSafe()
+    {
+        var inMemoryDatabase = new InMemoryDatabase();
+        inMemoryDatabase.AddData(key: 1, data: new BoardGameCacheRecord(Id: 1, Name: "Board1", Link: "https://www.lipsum.com/"));
+
+        var result1 = inMemoryDatabase.GetData(1).Name;
+        var result2 = inMemoryDatabase.GetData(1).Name;
+
+        Assert.Equal(result1, result2);
+    }
+    
+    [Fact]
+    public void DataRead_ShouldThowException()
+    {
+        var inMemoryDatabase = new InMemoryDatabase();
+        inMemoryDatabase.AddData(key: 1, data: new BoardGameCacheRecord(Id: 1, Name: "Board1", Link: "https://www.lipsum.com/"));
+
+        Assert.Throws<NotFoundRecordException>(() =>
+        {
+            _ = inMemoryDatabase.GetData(2).Name;
+        });
+    }
+}
